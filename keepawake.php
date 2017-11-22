0a90c114a3afba85c5e08d2c475b8ed7618cbe21
Matt Fannin <matt.fannin@powershop.co.nz>
2017-11-22 18:54:22 +1300

    Implement a method that doesn't cause volume drift over time.

--- keepawake.php
+++ keepawake.php
@@ -4,13 +4,23 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 
 $hk = new \HKAPI\API('192.168.1.25', 10025, new \HKAPI\Devices\AVR());
 
+echo "Wake\n";
 $hk->zone('Main Zone')->on();
-
 sleep(2);
 $hk->zone('Main Zone')->heartAlive();
 sleep(2);
-echo "Volume up\n";
-$hk->zone('Main Zone')->volumeUp();
+
+echo "Cycle display brightness ...\n";
+$hk->zone('Main Zone')->dim();
 sleep(2);
-echo "Volume down\n";
-$hk->zone('Main Zone')->volumeDown();
+
+echo "Medium\n";
+$hk->zone('Main Zone')->dim();
+sleep(2);
+
+echo "Off\n";
+$hk->zone('Main Zone')->dim();
+sleep(2);
+
+echo "On\n";
+$hk->zone('Main Zone')->dim();
